1d554f62d636cda6100e374cdd490557228a0745
MXQ <root@jojo-m.cn>
2020-05-16 15:07:24 +0800

    All required functions finished!

--- 医院预约挂号系统.cpp
+++ 医院预约挂号系统.cpp
@@ -1,6 +1,7 @@
 /*@Author MXQ
-work time 2020-05-05	05-10晚（预约普通门诊和专家门诊框架搭建）	5-11	5-14下午（patient类剩余功能完成）
-end time 2020
+start time about 2020-05-02
+work time 2020-05-05	05-10晚（预约普通门诊和专家门诊框架搭建）	5-11	5-14下午（patient类剩余功能完成） 5-16下午（所有基本要求功能完成）
+end time 2020-05-16
 */
 #include"System.h"
 int main() {
